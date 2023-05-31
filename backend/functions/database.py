f0ee17813c06780fe7139974eddb15da19af31be
EdisonWhale <edisonwhalex@gmail.com>
2023-05-31 01:22:59 -0400

    feature: done with backend part

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -48,3 +48,34 @@ def get_recent_messages():
         pass
 
     return messages
+
+# Save messages for retrieval later on
+
+
+def store_messages(request_message, response_message):
+
+    # Define the file name
+    file_name = "stored_data.json"
+
+    # Get recent messages
+    messages = get_recent_messages()[1:]
+
+    # Add messages to data
+    user_message = {"role": "user", "content": request_message}
+    assistant_message = {"role": "assistant", "content": response_message}
+    messages.append(user_message)
+    messages.append(assistant_message)
+
+    # Save the updated file
+    with open(file_name, "w") as f:
+        json.dump(messages, f)
+
+
+# Save messages for retrieval later on
+def reset_messages():
+
+    # Define the file name
+    file_name = "stored_data.json"
+
+    # Write an empty file
+    open(file_name, "w")
--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 import openai
 
+
 # Custiom Function Imports
 from functions.openai_requests import convert_audio_to_text, get_chat_response
-
-
+from functions.database import store_messages, reset_messages
+from functions.text_to_speech import convert_text_to_speech
 app = FastAPI()
 
 # CORS -Origins
@@ -29,6 +30,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 # Check Health
 
 
@@ -36,11 +38,23 @@ app.add_middleware(
 async def check_health():
     return {"message": "healthy"}
 
+# Reset Messages
 
-@app.get("/post-audio-get/")
-async def get_audio():
 
-    audio_input = open("voice.mp3", "rb")
+@app.get("/reset")
+async def reset_conversation():
+    reset_messages()
+    return {"message": "conversation reset"}
+
+
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+
+    # audio_input = open("voice.mp3", "rb")
+
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
 
     # Decode audio
     message_decoded = convert_audio_to_text(audio_input)
@@ -52,12 +66,24 @@ async def get_audio():
     # Get Chatgpt Response
     chat_response = get_chat_response(message_decoded)
 
+    # Guard: Ensure message decoded
+    if not chat_response:
+        return HTTPException(status_code=400, detail="Failed to get response")
+
+    # Store messages
+    store_messages(message_decoded, chat_response)
+
+    # Covert chat response to audio
     print(chat_response)
+    audio_output = convert_text_to_speech(chat_response)
 
-    return "Done"
+    # Guard: Ensure message decoded
+    if not audio_output:
+        return HTTPException(status_code=400, detail="Failed to get ElevenLabs audio response")
 
-# Post bot response
-# Note: Not playing in browser when using post request
-# @app.post("/post-audio")
-# async def post_audio(file:UploadFile = File(...)):
-#    print("hello")
+    # Create a generator that yields chunks of data
+    def iterfile():
+        yield audio_output
+
+    # Return audio file
+    return StreamingResponse(iterfile(), media_type="application/octet-stream")
